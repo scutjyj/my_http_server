f1d5cf0d38bf318f134e75a83c11f549dde487fe
scutjyj <704567300@qq.com>
2018-04-04 18:12:10 +0800

    1)handle the content type. 2)fix the starting failure about the argv bug. 3)handle the escape charactor in request url.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my_http_server
 make a http server without using any web framework.
+Some day, it will work as nginx or tornado.
--- my_http_server.py
+++ my_http_server.py
@@ -1,9 +1,12 @@
+# encoding: utf-8
+
 import time
 import os
 import sys
 import socket
 import threading
 import select
+import urllib
 
 HOST = '127.0.0.1'
 PORT = 800
@@ -18,13 +21,17 @@ HOME_PAGE_FILE = 'index.html'
 NOT_FOUND_PAGE = '404.html'
 
 
-def main_http_server(host=HOST, port=PORT):
+def main_http_server(host=HOST, port=PORT, *args):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, int(port)))
+    try:
+        s.bind((host, int(port)))
+    except:
+        print 'Please check the host and port!!!Make sure the port is not occupied!!!'
     s.listen(MAX_WAIT_NUM)
     print 'Start my http server!'
     read_list = [s]
+    thread_list = []
     while True:
         try:
             print 'Waiting for connection...'
@@ -33,12 +40,20 @@ def main_http_server(host=HOST, port=PORT):
                 # notice: the calling of accept() is blocking type.So use select module to implement the asynchronous programming.
                 cli_sock, cli_addr = _s.accept()
                 print 'the client:%s connect to my http server!' % str(cli_addr)
+                # TODO: Use thread.Event to solve the second problem in development_note.txt.
+                # TODO: Use logging module to save the log.
                 t = threading.Thread(target=handle_request, args=(cli_sock, cli_addr))
+                t.setName(cli_addr[0] + ':' + str(cli_addr[1]))
+                thread_list.append(t)
                 t.start()
         except KeyboardInterrupt:
             break
     print 'Stop my http server!'
-    t.join()
+    for t in thread_list:
+        thread_name = t.getName()
+        print 'stopping the thread[%s]...' % thread_name
+        t.join()
+        print 'the thread[%s] has been stopped!' % thread_name
     s.close()
 
 
@@ -48,10 +63,7 @@ def parse_request_header(request_header):
     try:
         ret_dict['request_method'], ret_dict['request_uri'], ret_dict['http_version'] = header_lines[0].split()
         for line in header_lines[1:]:
-            if 'Host' in line:
-                field_name, field_value = line.split(':', 1)
-            else:
-                field_name, field_value = line.split(':')
+            field_name, field_value = line.split(':', 1)
             ret_dict[field_name] = field_value.strip()
     except Exception as e:
         print 'invalid header!!!'
@@ -98,11 +110,13 @@ def handle_request(cli_sock, cli_addr):
     if len(request_header) > 0 and request_header[-4:] == REQUEST_HEADER_END:
         # parse request header.
         request_header_dict = parse_request_header(request_header[:-4])
-        # handle request.
+
+        # handle request.In fact, this is what application program should do.
         request_method = request_header_dict.get('request_method', '')
         if request_method == 'GET':
-            # TODO: What if the uri includes the escape charactor.
-            request_uri = request_header_dict.get('request_uri', '')
+            _request_uri = request_header_dict.get('request_uri', '')
+            # handle escape charactor.
+            request_uri = urllib.unquote(_request_uri)
             response_body_name = ''
             if request_uri:
                 # platform-specific.
@@ -120,7 +134,8 @@ def handle_request(cli_sock, cli_addr):
                 pass
             if response_body_name:
                 request_url = '{path_name}{file_name}'.format(path_name=STATIC_ROOT, file_name=response_body_name)
-                if os.path.exists(request_url):
+                print request_url
+                if os.path.exists(request_url) and not os.path.isdir(request_url):
                     http_response_code = '200'
                 else:
                     http_response_code = '404'
@@ -129,7 +144,9 @@ def handle_request(cli_sock, cli_addr):
         else:
             # TODO: handle other request methods.
             pass
+
         # send response.
+        # If it is a general web application framework, the code below can be encapsulated as API.
         if http_response_code == '404':
             if 'linux' in sys.platform:
                 path_separator = '/'
@@ -137,14 +154,32 @@ def handle_request(cli_sock, cli_addr):
                 path_separator = '\\\\'
             request_url = '{path_name}{separator}{file_name}'.format(path_name=STATIC_ROOT, separator=path_separator,
                                                                      file_name=NOT_FOUND_PAGE)
-        with open(request_url, 'r') as fp:
+        with open(request_url, 'rb') as fp:
             response_body_content = fp.read()
         content_length = len(response_body_content)
         response_desc_dict = {'200': 'OK', '404': 'Not Found'}
-        response_header_content = 'HTTP/1.1 {response_code} {response_desc}\r\nServer: jyj_web_server\r\nContent-Type: text/html\r\nContent-Length: {content_length}\r\nConnection: close\r\nVary: Accept-Encoding\r\n\r\n'
-        cli_sock.send(response_header_content.format(response_code=http_response_code,
+        # content type refers to the MIME standard.
+        content_type_dict = {'mp3': 'audio/mpeg', 'pdf': 'application/pdf', 'jpg': 'image/jpeg', 'css': 'text/css',
+                             'mp4': 'video/mp4'}
+        # find out the Content-Type.
+        _request_url = request_url.split('.')
+        if len(_request_url) == 2:
+            url_suffix = _request_url[1]
+        else:
+            url_suffix = ''
+        content_type = content_type_dict.get(url_suffix, 'text/html')
+        response_header_content = 'HTTP/1.1 {response_code} {response_desc}\r\nServer: jyj_web_server\r\nContent-Type: {content_type}\r\nContent-Length: {content_length}\r\nConnection: close\r\nVary: Accept-Encoding\r\n\r\n'
+        cli_sock.send(response_header_content.format(response_code=http_response_code, content_type=content_type,
                                                      response_desc=response_desc_dict[http_response_code],
                                                      content_length=content_length))
+        """
+        it seems we can not catch the KeyboardInterrupt exception here because the main thread has catched it?
+        try:
+            cli_sock.send(response_body_content)
+        except KeyboardInterrupt:
+            print 'get the KeyboardInterrupt signal!'
+        """
+        # TODO: what if the size of the file is too huge?
         cli_sock.send(response_body_content)
     else:
         # invalid request header.
@@ -152,34 +187,12 @@ def handle_request(cli_sock, cli_addr):
     cli_sock.close()
     print 'Finished handling the request from %s' % str(cli_addr)
 
-    """
-    with open('E:\\my_http_request_{sock}.txt'.format(sock=cli_addr[1]), 'w') as fp:
-        while True:
-            print 'before recving...'
-            try:
-                data = cli_sock.recv(1024)
-            except:
-                data = None
-            print data
-            print 'after recving...'
-            #time.sleep(1)
-            if data:
-                #print data
-                fp.write(data)
-                
-            else:
-                break
-    # send the http response header.
-    cli_sock.send('HTTP/1.1 200 OK\r\nServer: nginx\r\nDate: Fri, 30 Mar 2018 13:32:39 GMT\r\nContent-Type: text/html\r\nContent-Length: 52\r\nConnection: close\r\nLast-Modified: Fri, 30 Mar 2018 13:30:57 GMT\r\nVary: Accept-Encoding\r\nExpires: Fri, 30 Mar 2018 13:33:09 GMT\r\nCache-Control: max-age=60\r\n\r\n')
-    # send the http response content.
-    cli_sock.send('<html><head></head><body>Hello, world!</body></html>')
-    cli_sock.close()
-    print 'Finished handling the request from %s' % str(cli_addr)
-    """
-
 
 if __name__ == '__main__':
+    """
     if len(sys.argv) == 3:
         main_http_server(sys.argv[1], sys.argv[2])
     else:
         main_http_server()
+    """
+    main_http_server(*sys.argv[1:])
